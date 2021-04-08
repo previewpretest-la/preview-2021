d56405a028b4fe24c104cec5b2340983da8d128a
Federico <federico.izzop@gmail.com>
2021-04-08 16:47:06 +0200

    card accordion

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,40 @@
 document.getElementById("header").innerHTML = `
 
-      <div>
-PUT header HERE
-      </div>
+     <section class="websiteMargin">
+        <div class=" bg-yellow pt-5">
+            <div class="container-fluid ">
+                <div class="row justify-content-center pb-5">
+                    <img src="./assets/svg/logo_preview.svg" class="img-fluid w-25" alt="Responsive image">
+                </div>
+                <div class="row pb-3">
+                    <div class="col-3 d-flex justify-content-center">
+                        <div>
+                        <p class="display-7 text-decoration-underline cartoMonodB">QUANDO?</p>
+                        <p class="display-6 ibmSerifBold">25-26 Aprile 2020</p>
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex justify-content-center">
+                        <div>
+                        <p class="display-7 text-decoration-underline cartoMonodB">DOVE?</p>
+                        <p class="display-6 ibmSerifBold">25-26 Aprile 2020</p>
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex justify-content-center">
+                        <div>
+                        <p class="display-7 text-decoration-underline cartoMonodB">PER CHI?</p>
+                        <p class="display-6 ibmSerifBold">25-26 Aprile 2020</p>
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex justify-content-center">
+                        <div>
+                        <p class="display-7 text-decoration-underline cartoMonodB">CORSO DI STUDI?</p>
+                        <p class="display-6 ibmSerifBold">25-26 Aprile 2020</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 
       `
